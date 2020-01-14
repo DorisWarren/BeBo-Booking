c4dafa155b98b5cd1d374e4e8cf49d50ccabb925
DorisWarren <doriswarren11@gmail.com>
2020-01-14 13:41:01 -0800

    Add schemas and Resolvers - API is in working condition

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
 // Created Graph QL Server 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const graphqlHttp = require('express-graphql');
-// const {buildSchema} = require('graphql');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// app.use(
-//     '/graphql', 
-//     graphqlHttp({
-//         schema: buildSchema(`
-//             type RootQuery {
-//                 events: [String!]!
-//             }
+app.use(
+    '/graphql', 
+    graphqlHttp({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
 
-//             type RootMutation {
-//                 createEvent(name: String): String
-//             }
+            type RootMutation {
+                createEvent(name: String): String
+            }
 
-//             schema {
-//                 query: RootQuery
-//                 mutation: RootMutation
-//             }
-//         `),
-//         rootValue: {
-//         events: () => {
-//             return ['StartUp', 'Bebo Store', 'House Event']
-//         },
-//         createEvent: (args) => {
-//             const eventName = args.name;
-//             return eventName;
-//         }
-//     },
-//     graphiql: true
-//   })
-// );
-app.get('/',(req ,res, next) => {
-    res.send('Hello World');
-})
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+          events: () => {
+            return ['StartUp', 'Store', 'Private'];
+          },
+          createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+          }
+        },
+        graphiql: true
+      })
+    );
 
 app.listen(3000);
